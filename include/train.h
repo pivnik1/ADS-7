509277f3d7532447bad5783d5d95d7bc0e96c28e
pivnik1 <niga.gurunov38@mail.ru>
2025-06-06 16:17:21 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -11,10 +11,13 @@ class Train {
   };
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
   Car *first; // точка входа в поезд (первый вагон)
+  Car *current;
  public:
   Train();
+  ~Train();
   void addCar(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
+  void resetOps();
 };
 #endif  // INCLUDE_TRAIN_H_
